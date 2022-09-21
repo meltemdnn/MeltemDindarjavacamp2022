f8132439772499b35f8a33b5d43dbdeb6742a3d1
meltemdnn <54101227+meltemdnn@users.noreply.github.com>
2022-09-21 09:58:19 +0300

    Add files via upload

---
+++ datatypes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi = 12;
+        double sayi2 = 12.5;
+        sayi = - 129;
+
+        String sehir = "ANKARA";
+        char karakter = 'A';
+
+        boolean dogruMu= false;
+
+    }
+}
---
+++ variables/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+// reusability
+    public static void main(String[] args) {
+        int ogrenciSayisi = 12;
+        String mesaj = "Öğrenci Sayısı : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+        System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+        System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+
+
+    }
+
+
+}
